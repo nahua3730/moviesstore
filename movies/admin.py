7660ab0a5ec83e781f58a357fe7848587bf10124
nahua3730 <nahua3730@gmail.com>
2026-01-31 20:32:48 -0500

    Added report and admin control function

--- movies/admin.py
+++ movies/admin.py
@@ -5,5 +5,12 @@ from .models import Movie, Review
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name']
+class ReviewAdmin(admin.ModelAdmin):
+    list_display=('id', 'movie', 'user', 'comment', 'is_reported', 'date')
+    list_filter=['is_reported']
+    actions=['restore_reviews']
+    def restore_reviews(self, request, queryset):
+        queryset.update(is_reported=False)
+        self.message_user(request, "Selected reviews have been restored.")
 admin.site.register(Movie, MovieAdmin)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin) 
